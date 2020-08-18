b1f6d719afe73be6b02cb3b7b83bddc731294034
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-08-17 20:48:17 -0500

    creating an array of objects that contain the menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 console.log("JavaScript is running...");
 
+const menu = [
+  {
+    id: 0,
+    name: "Eggs"
+  }
+]
+
 let element = document.getElementById("item-00");
-let text = document.createTextNode("Item 0");
+let text = document.createTextNode(menu[0].name);
 element.appendChild(text);
